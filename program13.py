bcbf3092aa90a74d19dc9a471b73152925d9c24b
Jatin Satani <jatinsatani111@gmail.com>
2023-12-25 15:22:59 +0530

    Done

---
+++ program13.py
@@ -0,0 +1,11 @@
+original_set = {1, 2, 3, 4, 5,85}
+
+
+shallow_copy_set1 = original_set.copy()
+
+shallow_copy_set2 = set(original_set)
+
+
+print("Original Set:", original_set)
+print("Shallow Copy Set 1:", shallow_copy_set1)
+print("Shallow Copy Set 2:", shallow_copy_set2)
