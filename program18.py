e3ed6de035983f16d5af937a717d5bee8843203d
Jatin Satani <jatinsatani111@gmail.com>
2023-12-25 15:32:08 +0530

    Done

---
+++ program18.py
@@ -0,0 +1,8 @@
+
+num = int(input("Enter an integer: "))
+
+
+if num % 5 == 0:
+    print("number is a multiple of 5.")
+else:
+    print("number is not a multiple of 5.")
---
+++ program19.py
@@ -0,0 +1,8 @@
+
+num = int(input("Enter an integer: "))
+
+
+if num % 5 == 0 and num % 7 == 0:
+    print("number is a multiple of both 5 and 7.")
+else:
+    print("number is not a multiple of both 5 and 7.")
