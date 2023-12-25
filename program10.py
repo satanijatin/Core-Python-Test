c4782fd56530e0df75fe2bdd3d061b13e69b7e53
Jatin Satani <jatinsatani111@gmail.com>
2023-12-25 15:16:41 +0530

    Done

---
+++ program10.py
@@ -0,0 +1,7 @@
+set1={1,2,4,5,6,78,9,45}
+set2={4,5,2,5,7,89,0,9,7}
+
+a=set1.union(set2)
+b=set2.union(set1)
+print(a)
+print(b)
---
+++ program9.py
@@ -0,0 +1,7 @@
+set1={1,2,4,5,6,78,9,45}
+set2={4,5,2,5,7,89,0,9,7}
+
+a=set1.intersection(set2)
+b=set2.intersection(set1)
+print(a)
+print(b)
