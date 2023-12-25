9e01db285f2c872c2e94be3b87d112b815340a49
Jatin Satani <jatinsatani111@gmail.com>
2023-12-25 15:12:17 +0530

    Done

---
+++ program7.py
@@ -0,0 +1,6 @@
+addmember={32,34,54,65,76,87,98,4,3,54,4}
+print(addmember)
+
+
+addmember.add("12345")
+print(addmember)
---
+++ program8.py
@@ -0,0 +1,5 @@
+addmember={32,34,54,65,76,87,98,4,3,54,4}
+
+print(addmember)
+addmember.remove(34)
+print(addmember)
